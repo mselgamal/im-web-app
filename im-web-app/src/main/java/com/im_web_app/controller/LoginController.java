06d42bac8aed7a6c22c14841256ca101b167c822
Mamdouh Elgamal <mselgamal2@gmail.com>
2019-09-03 13:36:58 -0400

    added login page controller

---
+++ im-web-app/src/main/java/com/im_web_app/controller/LoginController.java
@@ -0,0 +1,13 @@
+package com.im_web_app.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+	@GetMapping("/login")
+	public String showLogin() {
+		return "login";
+	}
+}

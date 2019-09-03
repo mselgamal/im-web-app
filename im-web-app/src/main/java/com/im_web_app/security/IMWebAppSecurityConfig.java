3b79ff6f40818f5bbc50a65da1b5e4fda105b995
Mamdouh Elgamal <mselgamal2@gmail.com>
2019-09-03 13:34:52 -0400

    implemented different login url in security config

--- im-web-app/src/main/java/com/im_web_app/security/IMWebAppSecurityConfig.java
+++ im-web-app/src/main/java/com/im_web_app/security/IMWebAppSecurityConfig.java
@@ -2,6 +2,7 @@ package com.im_web_app.security;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
@@ -22,4 +23,14 @@ public class IMWebAppSecurityConfig extends WebSecurityConfigurerAdapter {
 				.password("mando").roles("ADMIN"));
 	}
 
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		
+		// setup authentication to different login page
+		http.authorizeRequests().anyRequest().authenticated()
+			.and().formLogin()
+				.loginPage("/login")
+				.loginProcessingUrl("/authenticateUser")
+				.permitAll();
+	}
 }

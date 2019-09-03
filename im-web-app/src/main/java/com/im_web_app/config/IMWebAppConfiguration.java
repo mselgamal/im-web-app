96a0dc230478e8bfb507336cef6b6f5e3d57439c
Mamdouh Elgamal <mselgamal2@gmail.com>
2019-09-03 11:16:26 -0400

    initial commit

---
+++ im-web-app/src/main/java/com/im_web_app/config/IMWebAppConfiguration.java
@@ -0,0 +1,25 @@
+package com.im_web_app.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages="com.im_web_app")
+public class IMWebAppConfiguration {
+	
+	// view resolver
+	@Bean
+	public ViewResolver viewResolver() {
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		
+		viewResolver.setPrefix("/WEB-INF/view/");
+		viewResolver.setSuffix(".jsp");
+		
+		return viewResolver;
+	}
+}

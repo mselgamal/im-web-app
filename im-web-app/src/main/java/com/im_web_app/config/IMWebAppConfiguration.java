fe1e499dad6863ab044acefbc65b6f41dc0d7863
Mamdouh Elgamal <mselgamal2@gmail.com>
2019-09-03 22:19:28 -0400

    added bootstrap form and resources path support

--- im-web-app/src/main/java/com/im_web_app/config/IMWebAppConfiguration.java
+++ im-web-app/src/main/java/com/im_web_app/config/IMWebAppConfiguration.java
@@ -5,12 +5,14 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages="com.im_web_app")
-public class IMWebAppConfiguration {
+public class IMWebAppConfiguration implements WebMvcConfigurer{
 	
 	// view resolver
 	@Bean
@@ -21,5 +23,11 @@ public class IMWebAppConfiguration {
 		viewResolver.setSuffix(".jsp");
 		
 		return viewResolver;
+	}
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/resources/**")
+				.addResourceLocations("/resources/");
 	}
 }

3f9d66a1637f0f3e76f9f200e210fa7ca3e1ba56
Mamdouh Elgamal <mselgamal2@gmail.com>
2019-09-10 13:45:37 -0400

    added jdbc auth, removed in-memmory auth

--- im-web-app/src/main/java/com/im_web_app/security/IMWebAppSecurityConfig.java
+++ im-web-app/src/main/java/com/im_web_app/security/IMWebAppSecurityConfig.java
@@ -1,26 +1,34 @@
 package com.im_web_app.security;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.User.UserBuilder;
 
 @Configuration
 @EnableWebSecurity
 public class IMWebAppSecurityConfig extends WebSecurityConfigurerAdapter {
 
+	@Autowired
+	private DataSource securityDataSource;
+	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		// temp in-memmory authentication for testing
 		
+		// db authentication
+		auth.jdbcAuthentication().dataSource(securityDataSource);
+		
+		// temp in-memmory authentication for testing
+		/*
 		@SuppressWarnings("deprecation")
 		UserBuilder users = User.withDefaultPasswordEncoder();
 		
 		auth.inMemoryAuthentication().withUser(users.username("mando")
-				.password("mando").roles("ADMIN"));
+				.password("mando").roles("ADMIN"));*/
 	}
 
 	@Override

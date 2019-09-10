0ec51dcfa6c90d14c50227d632c0db37c354095d
Mamdouh Elgamal <mselgamal2@gmail.com>
2019-09-10 13:21:47 -0400

    add db auth, connection and properties bean

--- im-web-app/src/main/java/com/im_web_app/config/IMWebAppConfiguration.java
+++ im-web-app/src/main/java/com/im_web_app/config/IMWebAppConfiguration.java
@@ -1,18 +1,70 @@
 package com.im_web_app.config;
 
+
+import java.beans.PropertyVetoException;
+import java.util.logging.Logger;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages="com.im_web_app")
+@PropertySource("classpath:persistence-mysql.properties")
 public class IMWebAppConfiguration implements WebMvcConfigurer{
+	
+	// represents enviroment vars extracted from properties file
+	@Autowired
+	private Environment env;
+	
+	private Logger logger = Logger.getLogger(getClass().getName());
+	
+	// db connection setup
+	@Bean
+	public DataSource securityDataSource() {
+		// create connection pool
+		ComboPooledDataSource securityDataSource = new ComboPooledDataSource();
+		
+		// set jdbc driver
+		try {
+			securityDataSource.setDriverClass(env.getProperty("jdbc.driver"));
+		} catch (PropertyVetoException exc) {
+			throw new RuntimeException(exc);
+		}
+		
+		logger.info(">>> JDBC.url: " + env.getProperty("jdbc.url"));
+		logger.info(">>> JDBC.user: " + env.getProperty("jdbc.user"));
+		
+		// set database connection properties
+		securityDataSource.setJdbcUrl(env.getProperty("jdbc.url"));
+		securityDataSource.setUser(env.getProperty("jdbc.user"));
+		securityDataSource.setPassword(env.getProperty("jdbc.password"));
+		
+		// set connection pool props
+		securityDataSource.setInitialPoolSize(Integer.parseInt(
+				env.getProperty("connection.pool.initialPoolSize")));
+		securityDataSource.setMinPoolSize(Integer.parseInt(
+				env.getProperty("connection.pool.minPoolSize")));
+		securityDataSource.setMaxPoolSize(Integer.parseInt(
+				env.getProperty("connection.pool.maxPoolSize")));
+		securityDataSource.setMaxIdleTime(Integer.parseInt(
+				env.getProperty("connection.pool.maxIdleTime")));
+		
+		return securityDataSource;
+	}
 	
 	// view resolver
 	@Bean
@@ -24,7 +76,8 @@ public class IMWebAppConfiguration implements WebMvcConfigurer{
 		
 		return viewResolver;
 	}
-
+	
+	// add static resources path, for js/css/imgs etc
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry.addResourceHandler("/resources/**")

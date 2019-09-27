0ef9b7f3a56d407e3daa8e4eabda356e4fbcb5e7
Mamdouh Elgamal <mselgamal2@gmail.com>
2019-09-27 10:16:34 -0400

    added spring-boot support files

---
+++ im-web-app/src/test/java/com/im_web_app/ImWebAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.im_web_app;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ImWebAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}

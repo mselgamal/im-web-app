b6531c7364756c7bccaa0a3beb71baf4319a39dc
Mamdouh Elgamal <mamdouh.elgamal@sciencelogic.com>
2019-11-06 11:09:11 -0500

    initial commit

--- im-web-app/db_creation_scripts/create-user.sql
+++
@@ -1,5 +0,0 @@
-CREATE USER 'chatadmin'@'localhost' IDENTIFIED BY 'chatadmin';
-
-GRANT ALL PRIVILEGES ON * . * TO 'chatadmin'@'localhost';
-
-ALTER USER 'chatadmin'@'localhost' IDENTIFIED WITH mysql_native_password BY 'chatadmin';
